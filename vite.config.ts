984349ec35ed42d2b88e8bc5a78a2e85fb1f5f97
Marek Šutý <mareksuty123@gmail.com>
2024-11-24 13:43:33 +0100

    Fix deploay

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MnaucinWatch/', // Should match your repository name
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './', // Change to relative path
   publicDir: 'public',
   build: {
+    outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
-        assetFileNames: 'assets/[name].[ext]'
+        assetFileNames: (assetInfo) => {
+          const info = (assetInfo.name ?? '').split('.');
+          const ext = info[info.length - 1];
+          if (assetInfo.name && /\.(css)$/i.test(assetInfo.name)) {
+            return `css/[name]-[hash][extname]`;
+          }
+          if (assetInfo.name && /\.(png|jpe?g|svg|gif|tiff|bmp|ico)$/i.test(assetInfo.name)) {
+            return `images/[name]-[hash][extname]`;
+          }
+          return `[name]-[hash][extname]`;
+        }
       }
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
     }
   },
   server: {
